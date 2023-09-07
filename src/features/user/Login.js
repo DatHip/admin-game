cd7af92e0dff65a57b3d3ea64005df522d5518f2
DatHip <83833987+DatHip@users.noreply.github.com>
2023-09-07 10:51:42 +0700

    feat: Auth

--- src/features/user/Login.js
+++ src/features/user/Login.js
@@ -1,32 +1,47 @@
 import {useState, useRef} from 'react'
 import {Link} from 'react-router-dom'
-import LandingIntro from './LandingIntro'
 import ErrorText from  '../../components/Typography/ErrorText'
 import InputText from '../../components/Input/InputText'
+import { apiPost} from '../../utils/https/request'
 
 function Login(){
 
     const INITIAL_LOGIN_OBJ = {
         password : "",
-        emailId : ""
+        username : "",
+        mode: 0,
     }
 
     const [loading, setLoading] = useState(false)
     const [errorMessage, setErrorMessage] = useState("")
     const [loginObj, setLoginObj] = useState(INITIAL_LOGIN_OBJ)
 
-    const submitForm = (e) =>{
+    const submitForm = async (e) =>{
         e.preventDefault()
         setErrorMessage("")
 
-        if(loginObj.emailId.trim() === "")return setErrorMessage("Email Id is required! (use any value)")
+        if(loginObj.username.trim() === "")return setErrorMessage("Email Id is required! (use any value)")
         if(loginObj.password.trim() === "")return setErrorMessage("Password is required! (use any value)")
         else{
             setLoading(true)
             // Call API to check user credentials and save token in localstorage
-            localStorage.setItem("token", "DumyTokenHere")
+            // localStorage.setItem("token", "DumyTokenHere")
+
+            try {
+                const res = await apiPost('login' , loginObj)
+                console.log(res)
+                if (res?.error === 0) {
+
+                } else {
+                
+                }
+            } catch (error) {
+                console.log(error)
+            }
+
+
             setLoading(false)
-            window.location.href = '/app/welcome'
+            // window.location.href = '/app/welcome'
         }
     }
 
@@ -36,34 +51,29 @@ function Login(){
     }
 
     return(
-        <div className="min-h-screen bg-base-200 flex items-center">
-            <div className="card mx-auto w-full max-w-5xl  shadow-xl">
-                <div className="grid  md:grid-cols-2 grid-cols-1  bg-base-100 rounded-xl">
-                <div className=''>
-                        <LandingIntro />
-                </div>
-                <div className='py-24 px-10'>
-                    <h2 className='text-2xl font-semibold mb-2 text-center'>Login</h2>
+        <div className="flex items-center min-h-screen bg-base-200">
+            <div className="w-full max-w-2xl mx-auto shadow-xl card">
+                <div className='px-10 py-24'>
+                    <h2 className='mb-2 text-2xl font-semibold text-center'>Login</h2>
                     <form onSubmit={(e) => submitForm(e)}>
 
                         <div className="mb-4">
 
-                            <InputText type="emailId" defaultValue={loginObj.emailId} updateType="emailId" containerStyle="mt-4" labelTitle="Email Id" updateFormValue={updateFormValue}/>
+                            <InputText type="username" defaultValue={loginObj.username} updateType="username" containerStyle="mt-4" labelTitle="User Name" updateFormValue={updateFormValue}/>
 
                             <InputText defaultValue={loginObj.password} type="password" updateType="password" containerStyle="mt-4" labelTitle="Password" updateFormValue={updateFormValue}/>
 
                         </div>
 
-                        <div className='text-right text-primary'><Link to="/forgot-password"><span className="text-sm  inline-block  hover:text-primary hover:underline hover:cursor-pointer transition duration-200">Forgot Password?</span></Link>
+                        <div className='text-right text-primary'><Link to="/forgot-password"><span className="inline-block text-sm transition duration-200 hover:text-primary hover:underline hover:cursor-pointer">Forgot Password?</span></Link>
                         </div>
 
                         <ErrorText styleClass="mt-8">{errorMessage}</ErrorText>
                         <button type="submit" className={"btn mt-2 w-full btn-primary" + (loading ? " loading" : "")}>Login</button>
 
-                        <div className='text-center mt-4'>Don't have an account yet? <Link to="/register"><span className="  inline-block  hover:text-primary hover:underline hover:cursor-pointer transition duration-200">Register</span></Link></div>
+                        <div className='mt-4 text-center'>Don't have an account yet? <Link to="/register"><span className="inline-block transition duration-200 hover:text-primary hover:underline hover:cursor-pointer">Register</span></Link></div>
                     </form>
                 </div>
-            </div>
             </div>
         </div>
     )
---
+++ src/utils/https/request.js
@@ -0,0 +1,36 @@
+
+import axios from 'axios';
+
+export async function apiPost(url, payload, header = {}) {
+  try {
+    const { data } = await axios.post(url, payload, {
+      headers: header,
+    });
+    return data;
+  } catch (error) {
+    console.log('🚀 ~ apiPost ~ error:', error);
+    const response = {
+      data: undefined,
+      error: 1,
+      message: 'system_error',
+    };
+    return response;
+  }
+}
+
+export async function apiGet(url, header = {}) {
+  try {
+    const { data } = await axios.get(url, {
+      headers: header,
+    });
+    return data;
+  } catch (error) {
+    console.log('🚀 ~ apiGet ~ error:', error);
+    const response = {
+      data: undefined,
+      error: 1,
+      message: 'system_error',
+    };
+    return response;
+  }
+}
